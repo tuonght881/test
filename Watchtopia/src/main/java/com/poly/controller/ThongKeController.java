d85aff70b0a91c1740459e2429eba124cd3e0150
thien <trungthien03s@gmail.com>
2023-06-11 21:03:57 +0700

    trung thien Merge remote-tracking branch 'origin/main_v2'

--- Watchtopia/src/main/java/com/poly/controller/ThongKeController.java
+++ Watchtopia/src/main/java/com/poly/controller/ThongKeController.java
@@ -1,15 +1,24 @@
 package com.poly.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.poly.DAO.ThongKeDAO;
+import com.poly.entity.ThongKe;
+
 @Controller
 public class ThongKeController {
-
+ThongKeDAO tkDao;
 	@RequestMapping("/admin/thongke")
 //	@ResponseBody
-	public String showTK() {
+	public String showTK(Model m) {
+		List<ThongKe> listTK = tkDao.getListTK();
+		m.addAttribute("listTK", listTK);
+		
 		return "/admin/statistic";
 	}
 }
