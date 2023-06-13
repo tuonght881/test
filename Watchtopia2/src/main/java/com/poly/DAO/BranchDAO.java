2880c03bddb9519a2dac1d519c5c5b88c2ab4f51
tuonght <tuongho881@gmail.com>
2023-06-13 20:56:00 +0700

    delete

--- Watchtopia2/src/main/java/com/poly/DAO/BranchDAO.java
+++
@@ -1,9 +0,0 @@
-package com.poly.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poly.entity.Branch;
-
-public interface BranchDAO extends JpaRepository<Branch, Integer>{
-
-}
--- Watchtopia2/src/main/java/com/poly/DAO/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.poly.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poly.entity.OrderDetail;
-
-public interface OrderDetailDAO extends JpaRepository<OrderDetail, Integer>{
-
-}
